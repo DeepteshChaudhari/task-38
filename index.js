2a7121dabfd320cfb40444781b4e4508f1cd2b9a
Deeptesh chaudhari <diptesh151097@gmail.com>
2023-10-13 09:17:30 +0530

    final

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
-const route = require("./route");
+require("dotenv").config();
+const roomsRouter = require("./routes/rooms.routes");
 
-require("dotenv").config(".env");
-
-app.use(cors({ origin: "*" }));
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(roomsRouter);
 
-app.use('/',route);
-
-app.use("*",(req,res)=>{
-    res.status(404).send("Page Not Found")
-});
-
-const PORT = process.env.PORT||5000;
-app.listen(PORT,()=>{
-    console.log(`Server listnening on PORT ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`App is running on port ${PORT}`);
 });
